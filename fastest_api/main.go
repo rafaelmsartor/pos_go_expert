4847dc467513ff672c3e51d8ce65b3d48884b196
Rafael Sartor <rafaelmsartor@gmail.com>
2024-06-25 21:54:04 -0300

    Solution for the multithreading exercise

---
+++ fastest_api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+const brasilAPI = "https://brasilapi.com.br/api/cep/v1/%s"
+const viacepAPI = "http://viacep.com.br/ws/%s/json/"
+
+func main() {
+	// read the CEP from the command line
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a CEP as an argument")
+		return
+	}
+	cep := os.Args[1]
+
+	first_channel := make(chan string)
+	second_channel := make(chan string)
+	go makeRequest(brasilAPI, cep, first_channel)
+	go makeRequest(viacepAPI, cep, second_channel)
+
+	// wait for the responses
+	select {
+	case res := <-first_channel:
+		fmt.Println("Received the response from BrasilAPI first")
+		fmt.Println(res)
+	case res := <-second_channel:
+		fmt.Println("Received the response from ViaCEP first")
+		fmt.Println(res)
+	}
+}
+
+func makeRequest(api, cep string, ch chan string) {
+	req, err := http.Get(fmt.Sprintf(api, cep))
+	if err != nil {
+		fmt.Println("Error making the request to ", api, err)
+		return
+	}
+	defer req.Body.Close()
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("Error reading the response from ", api, err)
+		return
+	}
+	ch <- string(res)
+}
